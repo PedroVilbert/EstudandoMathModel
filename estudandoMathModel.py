3f53dbd57c7c151b0d1392d0c7f99d09ca349418
PedroVilbert <pedrovilbert2005@gmail.com>
2025-05-29 17:00:11 -0300

    Tentando adicionar icones

--- estudandoMathModel.py
+++ estudandoMathModel.py
@@ -3,6 +3,7 @@ from matmodel.util.parsers import df2trajectory
 import pandas as pd
 from matmodel.base import Space2D
 import plotly.express as px
+from PIL import Image
 
 
 ds = 'mat.FoursquareNYC'
@@ -17,15 +18,7 @@ traj = T[0] #T[1] é uma pessoa especifica, caso mude o valor, muda a pessoa
     6. root_type (nominal), 7. rating (numeric), 8. weather (nominal)
     
 """
-"""
-print(data_desc.attributes) # Lista os atributos
-for point in traj.points:
-    print(point.seq)
-# for i in traj.points: # Traj.points é as trajetorias que a pessoa do T[1] fez
-#     #if i.aspects[5].value == 'Travel & Transport' and i.aspects[2].value == "Monday": # Tem que usar i.value para acessar o valor do aspect
-#         print("\npoi",i.aspects) # .aspects[3] é os lugares por onde a pessoa passou
 
-"""
 def mapa():
     # Extrai coordenadas (assumindo que aspects[0].x = latitude e aspects[0].y = longitude)
     coordenadas = [] # array com as coodenadas e nome da localização
@@ -36,6 +29,9 @@ def mapa():
 
         #Função para convereter o valor numerico em estrelas para a valiação 
         avaliacao = notas_mapa(point.aspects[6].value) 
+
+        #Função para converter o clima em icone
+        icone = icones_clima(point.aspects[7].value)
         
         #Adiciona no array as coordenadas com os vatributos
         coordenadas.append({
@@ -43,8 +39,8 @@ def mapa():
             "lon": point.aspects[0].y,  # Longitude (eixo Y) 
             "text": str(point.aspects[3]), # Nome da localização
             "time": str(point.aspects[1]), # Horario do checkin
-            "weather": str(point.aspects[7]), #clima
-            "rating": avaliacao,
+            "weather": icone, #clima
+            "rating": avaliacao, # Avaliações em formato de '*' ou '-' caso seja não tenha nota 
             "point": point.seq,
         })
 
@@ -63,10 +59,13 @@ def mapa():
         mapbox_style="open-street-map",  # Estilo do mapa (pode ser "stamen-terrain", "carto-positron", etc.)
         height=700, #Tamanho do mapa
     )   
-            
+        
     # Conectar os pontos com linhas
-    fig.update_traces(marker=dict(size=8, symbol= 'circle'), line=dict(width=2, color='black'),mode='lines+markers')
+    fig.update_traces(marker=dict(size=8, symbol= 'circle', image=dict(src=icone,opacity=0.8,width=20,height=20) ), #Erro na imagem !!!!!!!!! 
+                      line=dict(width=2, color='black'),
+                      mode='lines+markers')
     fig.show()
+
 
 
 def notas_mapa(nota):
@@ -84,6 +83,25 @@ def notas_mapa(nota):
         
         return avaliacao #Retorna a avaliação
 
+
+def icones_clima(clima_ponto):
+    
+    # icon_nuvem = ('https://cdn-icons-png.flaticon.com/128/362/362406.png')
+    # icon_sol = ('https://cdn-icons-png.flaticon.com/128/9055/9055356.png')
+    
+    
+    clima = {
+        "Clouds": 'https://cdn-icons-png.flaticon.com/128/362/362406.png',
+        "Clear": 'https://cdn-icons-png.flaticon.com/128/9055/9055356.png'
+    }
+    
+    icone = clima.get(clima_ponto)
+    
+    return icone
+    
+    
+        
 print(data_desc.attributes) # Lista os atributos
+
 mapa()
 
